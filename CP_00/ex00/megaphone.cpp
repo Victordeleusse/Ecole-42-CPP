e2b0576b9579b7d2450c2f71315ce37d4780ea2f
Victor DE LEUSSE <vde-leus@e3r8p4.clusters.42paris.fr>
2023-04-14 15:48:38 +0200

    Avancement mode c++

--- CP_00/ex00/megaphone.cpp
+++ CP_00/ex00/megaphone.cpp
@@ -6,12 +6,26 @@
 /*   By: vde-leus <vde-leus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 12:43:11 by vde-leus          #+#    #+#             */
-/*   Updated: 2023/04/14 15:30:17 by vde-leus         ###   ########.fr       */
+/*   Updated: 2023/04/14 15:48:20 by vde-leus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
+
+// Utilisation d'un iterator et de ses parametres .begin() et .end()
+// On aurait pu egalement utiliser la propriete .length() de string avec i < str.length()
+
+std::string ft_capitalize(std::string str)
+{
+	std::string::iterator it = str.begin();
+	while (it != str.end())
+	{
+		*it = std::toupper(*it);
+		it++;
+	}
+	return (str);
+}
 
 int	main(int argc, char **argv)
 {
@@ -22,15 +36,8 @@ int	main(int argc, char **argv)
 	i = 1;
 	while (i < argc)
 	{
-		std::string str (argv[i]);
-		std::string::iterator it = str.begin();
-		while (it != str.end())
-		{
-			std::cout << *it;
-			it++;
-		}
+		std::cout << ft_capitalize((std::string)argv[i]);
 		i++;
 	}
-	std::cout << std::endl;
 	return (0);
 }
