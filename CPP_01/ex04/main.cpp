1db10c656f51cba767e3334900254017810cb03b
Victor DE LEUSSE <vde-leus@e3r9p20.clusters.42paris.fr>
2023-04-24 16:45:26 +0200

    Avancement CPP_01

--- CPP_01/ex04/main.cpp
+++ CPP_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vde-leus <vde-leus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 14:24:10 by vde-leus          #+#    #+#             */
-/*   Updated: 2023/04/24 16:18:18 by vde-leus         ###   ########.fr       */
+/*   Updated: 2023/04/24 16:44:55 by vde-leus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ int	main(void)
 {
 	std::ifstream		original("fichier.txt");
 	std::string			file_content;
-	std::vector<size_t>	positions;
+	std::vector<int>	positions;
 	std::string			str1 ("needle");
 	// std::string			str2 ("thorns");
-	size_t				idx = 0;
+	int				idx = 0;
 	
 	std::getline(original, file_content, '\0');
 	original.close();
@@ -28,18 +28,13 @@ int	main(void)
 	// std::cout << idx << std::endl;
 	if (idx != std::string::npos)
 		positions.push_back(idx);
-	while (1) 
+	while (idx < (file_content.length() - str1.length())) 
 	{
-		if (idx != std::string::npos && (str1[idx + 1]))
-		{
-			idx = file_content.find(str1, idx + 1);
-			positions.push_back(idx);
-			idx += str1.length();
-		}
-		else 
-			break ;
+		idx = file_content.find(str1, idx + 1);
+		positions.push_back(idx);
+		idx += str1.length();
 	}
-	std::vector<size_t>::iterator it = positions.begin();
+	std::vector<int>::iterator it = positions.begin();
 	while (it != positions.end())
 	{
 		std::cout << "position -> " << *it << std::endl;
